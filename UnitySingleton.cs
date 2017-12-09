4ad327059cf595c390558a2e003f29c4d9b9635c
Anthon Fredriksson <anthonnorrby@gmail.com>
2017-12-09 12:55:13 +0100

    Update UnitySingleton.cs

--- UnitySingleton.cs
+++ UnitySingleton.cs
@@ -1,32 +1,6 @@
 using UnityEngine;
 using System;
 
-/*
-    Use this class if you need to create a singleton.
-
-    Usage:
-    
-    [UnitySingleton(mustBeOnlyComponentOnObject: true, mustBeWithinRootObject: true)]
-    class MySingletonClass : UnitySingleton<MySingletonClass> {
-        protected overide void OnAwake() {
-            // mimics MonoBehaviour Awake
-        }
-    }
-
-    If "mustBeOnlyComponentOnObject" is true, then an error will be raised
-    if there are any other components on the GameObject other than a Transform and MySingletonClass
-
-    If "mustBeWithinRootObject" is true, then an error will be raised
-    if the GameObject MySingletonClass has a parent(which is equal to not being in the root object)
-
-    An error will also be raised if there are two or more of MySingletonClass in the scene.
-    Also, every instance of MySingletonClass but one is Destroyed upon Awake to ensure that only one MySingletonClass can remain active.
-
-    Why not use [DisallowMultipleComponent]? 
-    Its up to the user to add this to its own Singleton.
-    It will only prevent the editor from adding multiple.
-    However, if a component of the same time is added to the GameObject during runtime, mustBeOnlyInstanceInScene is needed.
-*/
 public abstract class UnitySingleton<T> : MonoBehaviour where T : MonoBehaviour {
     protected static T instance;
 
